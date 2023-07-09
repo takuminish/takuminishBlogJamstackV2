b62e7c5a0731732abfa4fef4ededab4cf36bd7b6
takuminish <takuminv1997@gmail.com>
2023-07-09 23:15:00 +0900

    content/blogのmarkdownを取得できるようにする

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,16 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/content/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
 
 export default config
