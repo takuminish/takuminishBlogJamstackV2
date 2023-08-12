f2f550cd033199337ba267a2d15ca148346529c5
takuminish <takuminv1997@gmail.com>
2023-08-13 00:21:14 +0900

    add: profileをmicroCMSから取得する

---
+++ src/features/portfolio/components/Experience.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
+import { format } from "date-fns";
+import { Experience } from "../../../pages";
+
+type Props = {
+  experiences: Experience[];
+};
+
+function History(props: Props) {
+  const { experiences } = props;
+
+  return (
+    <Timeline position="alternate">
+      {experiences.map((experience) => (
+        <TimelineItem>
+          <TimelineSeparator
+            sx={{
+              margin: "0 7px",
+            }}
+          >
+            <TimelineDot variant="outlined" color="success" />
+            <TimelineConnector sx={{ backgroundColor: "#2f7d31" }} />
+          </TimelineSeparator>
+          <TimelineContent
+            sx={{
+              boxShadow: "1px 1px 1px 1px #dddddd",
+              borderRadius: "10px",
+              height: "100%",
+            }}
+          >
+            {experience.title}{" "}
+            {format(new Date(experience.startAt), "yyyy/MM/dd")} ~{" "}
+            {experience.endAt
+              ? format(new Date(experience.endAt), "yyyy/MM/dd")
+              : ""}
+          </TimelineContent>
+        </TimelineItem>
+      ))}
+    </Timeline>
+  );
+}
+
+export default History;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,94 @@
 import * as React from "react";
 import { graphql, PageProps } from "gatsby";
 import Layout from "../shared/components/Layout";
+import Experience from "../features/portfolio/components/Experience";
+import { Box, Typography } from "@mui/material";
 
+export type Experience = {
+  detailedInformation: string;
+  endAt: string;
+  fieldId: string;
+  startAt: string;
+  title: string;
+};
 type DataProps = {
-  allMarkdownRemark: {
-    nodes: {
-      frontmatter: {
-        date: string;
-        description: string;
-        title: string;
-      };
-      html: string;
+  microcmsProfile: {
+    id: string;
+    licenses: {
+      fieldId: string;
+      name: string;
+      passedAt: Date;
+      isITLicense: boolean;
     }[];
+    products: {
+      fieldId: string;
+      name: string;
+      detailedInformation: string;
+    }[];
+    profile: {
+      fieldId: string;
+      name: string;
+      introduction: string;
+      githubUrl: string;
+      qiitaUrl: string;
+    };
+    skills: {
+      fieldId: string;
+      name: string;
+      detailedInfomation: string;
+      yearOfExperience: number;
+    }[];
+    experiences: Experience[];
   };
 };
 
-const BlogIndexPage = ({
-  data: { allMarkdownRemark },
-}: PageProps<DataProps>) => {
+const BlogIndexPage = ({ data: { microcmsProfile } }: PageProps<DataProps>) => {
   return (
     <Layout>
-      <div>
-        {allMarkdownRemark.nodes.map((node) => {
-          return <h1>{node.frontmatter.title}</h1>;
-        })}
-      </div>
+      <Box>
+        <Typography variant="h4" sx={{ fontWeight: 700, textAlign: "center" }}>
+          Experience
+        </Typography>
+        <Experience experiences={microcmsProfile.experiences} />
+      </Box>
     </Layout>
   );
 };
 
 export const query = graphql`
-  query IndexPageQuery {
-    allMarkdownRemark {
-      nodes {
-        frontmatter {
-          date
-          description
-          title
-        }
-        html
+  query MicroCMSProfileQuery {
+    microcmsProfile {
+      id
+      licenses {
+        fieldId
+        name
+        passedAt
+        isITLicense
+      }
+      products {
+        fieldId
+        name
+        detailedInformation
+      }
+      profile {
+        fieldId
+        name
+        introduction
+        githubUrl
+        qiitaUrl
+      }
+      skills {
+        fieldId
+        name
+        detailedInfomation
+        yearOfExperience
+      }
+      experiences {
+        fieldId
+        title
+        startAt
+        endAt
+        detailedInformation
       }
     }
   }
