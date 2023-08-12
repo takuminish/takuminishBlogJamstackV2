f00d4bfa50d0101c38b3ae20cdaa6e0b28c312ab
takuminish <takuminv1997@gmail.com>
2023-08-12 16:33:51 +0900

    add: ブログ一覧のレイアウトが完成

--- src/pages/blogs/index.tsx
+++ src/pages/blogs/index.tsx
@@ -1,15 +1,13 @@
 import * as React from "react";
-import { graphql, PageProps, Link } from "gatsby";
+import { graphql, PageProps } from "gatsby";
 import {
   Box,
-  Button,
   Card,
   CardActionArea,
-  CardActions,
   CardContent,
-  CardMedia,
   Typography,
 } from "@mui/material";
+import FolderIcon from "@mui/icons-material/Folder";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
 type DataProps = {
@@ -34,8 +32,9 @@ const BlogIndexPage = ({
       <Typography
         sx={{
           padding: "12px",
+          fontWeight: 700,
         }}
-        variant="h2"
+        variant="h3"
       >
         Blogs
       </Typography>
@@ -43,53 +42,79 @@ const BlogIndexPage = ({
         {allMarkdownRemark.nodes.map((node) => {
           return (
             <Grid2 key={node.id} xs={12} sm={6} md={4}>
-              <Card sx={{ backgroundColor: "#e0efff" }}>
-                <CardContent>
-                  <Box
-                    sx={{
-                      height: "190px",
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      backgroundColor: "#FFFFFF",
-                      borderRadius: "4px",
-                    }}
-                  >
+              <Card sx={{ backgroundColor: "#fffff" }}>
+                <CardActionArea href={node.id}>
+                  <CardContent>
                     <Box
-                      component="span"
                       sx={{
-                        height: "100%",
-                        fontSize: "100px",
-                        lineHeight: "190px",
+                        paddingBottom: "10px",
                       }}
                     >
-                      {node.frontmatter.icon}
+                      <Typography variant="h6">
+                        {node.frontmatter.title}
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {node.frontmatter.description}
+                      </Typography>
                     </Box>
-                  </Box>
-                  <Typography variant="h6">{node.frontmatter.title}</Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    {node.frontmatter.description}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button href={node.id} variant="outlined">
-                    OPEN
-                  </Button>
-                </CardActions>
+                    <Box
+                      sx={{
+                        paddingBottom: "10px",
+                      }}
+                    >
+                      <Box>
+                        <FolderIcon
+                          color="primary"
+                          sx={{
+                            verticalAlign: "middle",
+                          }}
+                        />
+                        <Box
+                          component="span"
+                          sx={{
+                            verticalAlign: "middle",
+                            color: "#37bdf8",
+                            fontSize: "13px",
+                            fontWeight: 700,
+                          }}
+                        >
+                          カテゴリー
+                        </Box>
+                      </Box>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          justifyContent: "start",
+                          gap: "5px",
+                        }}
+                      >
+                        <Box
+                          component="span"
+                          sx={{
+                            padding: "0 7px",
+                            backgroundColor: "#94a3b8",
+                            borderRadius: "10px",
+                            color: "#ffffff",
+                            fontSize: "13px",
+                            fontWeight: 700,
+                          }}
+                        >
+                          タグ
+                        </Box>
+                      </Box>
+                    </Box>
+                    <Box>
+                      <Typography variant="body2" color="text.secondary">
+                        createdAt: 2021-12-31
+                      </Typography>
+                    </Box>
+                  </CardContent>
+                </CardActionArea>
               </Card>
             </Grid2>
           );
         })}
       </Grid2>
-      <Typography
-        sx={{
-          padding: "12px",
-        }}
-        variant="h2"
-      >
-        Zenn
-      </Typography>
-      Coming Soon...
     </>
   );
 };
