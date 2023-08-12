68c3b43c466a31415add6a81e2941632e75cab41
takuminish <takuminv1997@gmail.com>
2023-08-12 18:30:30 +0900

    add: ブログのメタデータの表示

--- src/pages/blogs/{MarkdownRemark.id}.tsx
+++ src/pages/blogs/{MarkdownRemark.id}.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import { graphql, PageProps } from "gatsby";
 import Box from "@mui/material/Box";
+import ValueWrapper from "../../features/blogs/components/ValueWarapper";
+import Typography from "@mui/material/Typography";
+import DateText from "../../features/blogs/components/DateText";
+import Category from "../../shared/components/Category";
+import Tag from "../../shared/components/Tag";
 
 type DataProps = {
   markdownRemark: {
@@ -21,12 +26,37 @@ const BlogDetailPage = ({ data: { markdownRemark } }: PageProps<DataProps>) => {
         margin: "auto",
         display: "flex",
         justifyContent: "center",
+        flexDirection: "column",
         padding: "1rem 3rem 3rem 3rem",
         width: "80%",
       }}
     >
+      <Box sx={{ width: "100%", textAlign: "center" }}>
+        <ValueWrapper>
+          <Typography variant="h3" sx={{ fontWeight: 700 }}>
+            {markdownRemark.frontmatter.title}
+          </Typography>
+        </ValueWrapper>
+        <ValueWrapper>
+          <Category text="カテゴリー" />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              gap: "5px",
+            }}
+          >
+            <Tag text="タグ" />
+          </Box>
+        </ValueWrapper>
+        <ValueWrapper>
+          <DateText
+            label="createdAt"
+            date={new Date(markdownRemark.frontmatter.date)}
+          />
+        </ValueWrapper>
+      </Box>
       <article className="markdown-body">
-        <h1>{markdownRemark.frontmatter.title}</h1>
         <div
           dangerouslySetInnerHTML={{
             __html: markdownRemark.html,
