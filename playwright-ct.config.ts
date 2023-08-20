452123267981eb9bb0d8422ac552b41fdcb54eaa
takuminish <takuminv1997@gmail.com>
2023-08-20 18:25:34 +0900

    playwrightの設定

---
+++ playwright-ct.config.ts
@@ -0,0 +1,59 @@
+import { defineConfig, devices } from '@playwright/experimental-ct-react';
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './e2e',
+  /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
+  snapshotDir: './__snapshots__',
+  /* Maximum time one test can run for. */
+  timeout: 30000,
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://localhost:8000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    ctViteConfig: {
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./testSetup.ts'],
+        include: ['./**/*.test.(ts|tsx)'],
+        alias: [
+            {
+                find: /.+\.(jpg|jpeg|png|gif|svg|webp|)$/,
+                replacement: './__mocks__/fileMock.js'
+            }
+        ],
+        testTimeout: 100000
+      },
+    }
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+    webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:8000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 100000,
+  },
+});
